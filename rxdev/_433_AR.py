00b9f82dfab2d80a3fc6c1fe479cf0b2c8c784f0
hdtodd <hdtodd@gmail.com>
2022-03-22 21:34:26 -0400

    Working RXA609

--- rxdev/_433_AR.py
+++ rxdev/_433_AR.py
@@ -1,5 +1,7 @@
-#!/usr/bin/env python
+#!/usr/local/bin/python3.10
 # _433_AR.py
+
+#Version 1: uses lists rather than classes
 
 '''
 This module provides two classes to use with wireless 433MHz 
@@ -32,222 +34,254 @@ The transmit function allows the timings to be set as parameters,
     to send/receive 40-bit Acurite packets
 '''
 
+import time
+import pigpio
+
+# machine states
+SYNC_WAIT    = 0
+DATA_COLLECT = 1
+PKT_GAP_WAIT = 2
+States = ["SYNC_WAIT", "DATA_COLLECT", "PKT_GAP_WAIT"]
+
+# interval types
+SYNC_GAP = 0
+PULSE    = 1
+SHORT    = 2
+LONG     = 3
+SYNC     = 4
+GAP      = 5
+Intervals = ["SYNC_GAP", "PULSE", "SHORT", "LONG", "SYNC", "GAP"]
+
 # set default waveform parameters for Acurite 609 transmission timings
 #  These are scaled after sending & receiving a test pulse train
 #  The pigpio library timings vary depending upon system configuration,
 #  So this library provides a calibration function to check
 #  actual timings vs requested timings for pulses.
-PULSE_WIDTH =  425
-SHORT       = 1006
-LONG        = 2000
-SYNC        = 8940
-GAP         =10200
+Timing_Table = [
+   [SYNC_GAP  , "SYNC_GAP",  475, 0, 0],       #interval between sync pulses
+   [PULSE     , "PULSE"   ,  505, 0, 0],       #pulses are this duration, +/- TOLERANCE
+   [SHORT     , "SHORT"   , 1006, 0, 0],       #interval after pulse to indicate data bit "0"
+   [LONG      , "LONG"    , 2000, 0, 0],       #interval after pulse to indicate data bit "1"
+   [SYNC      , "SYNC"    , 8240, 0, 0], #8940      #interval after third sync pulse, before first data
+   [GAP       , "GAP"     ,10200, 0, 0]        #interval after pulse that terminates last data bit
+   ]
+
 MSGLEN      =   40
 REPEATS     =    3
 
-MICROS      =  500    #timing for calibration: 500usec high-low pulse
+#IMPORTANT TO CHECK 433MHz receiver output to see if "on" is 0v0 or 3v3;
+#  Trailing should be set to 1 if pulses are 3v3-->0v0 (inverted)
+TRAILING    =    1           #Set to 0 if pulse voltages are 0->1 or to 1 if they're 1->0 
+MICROS      =  500           #Timing for calibration: 500usec high-low pulse
 
-import time
-import pigpio
+TOLERANCE   =   17           #Timing tolerance for edge classification (as %)
 
-"""
-   rx: A class to read wireless codes transmitted by 433 MHz transmitter
-"""
+#This is the state-machine recognizer for Acurite PPM packets
+#Its _next() function  accepts a token that indicates the type of
+#  "edge" of length "interval" microsec just received
+#  and advances the machine state, depending on current state and token type
+class mach():
+   def __init__(self,callback=None):
+      self.cb = callback
+      self._reset()
+      
+   def _reset(self):
+      self.state = SYNC_WAIT
+      self.sync_count = 0
+      self.bit_count  = 0
+      self.code       = 0
+      self.pulseavg   = 0
+      self.pulsecnt   = 0
+      self.shortavg   = 0
+      self.shortcnt   = 0
+      self.longavg    = 0
+      self.longcnt    = 0
+      self.need_pulse = True
+      
+   def _next(self,token,interval=1):
+      #first, accumulate metrics for possible analysis
+      if token == PULSE:
+         self.pulseavg += interval
+         self.pulsecnt += 1
+         if not self.need_pulse:    #two pulses in a row?  No way.
+            self._reset()
+            return
+         self.need_pulse = False
+         return
+      elif token == SHORT:
+         self.shortavg += interval
+         self.shortcnt += 1
+      else:
+         self.longavg  += interval
+         self.longcnt  += 1
+
+      if self.need_pulse:
+#         print("got an interval when we expected a pulse; reset")
+         self._reset()
+         return
+
+      #now evaluate state change: we have just a small number of valid transitions
+      if ( self.state == SYNC_WAIT and token == SYNC_GAP ):
+         self.sync_count += 1
+#         print("Checking SYNC_WAIT and SYNC_GAP; sync_count = ", self.sync_count)
+         if self.sync_count >=3:
+            #should be 2 pulse+sync-gaps then 1 pulse+sync-interval
+            #  so 3 pulst+sync-gaps isn't our patern: reset machine
+            self._reset()
+         self.need_pulse = True
+         return
+
+      if ( self.state == SYNC_WAIT and token == SYNC ):
+         if self.sync_count==2:
+            #We've seen two sync pulse/gap pairs, and now a pulse/SYNC interval
+            # So sync pattern is complete: go into data collect mode
+            self.state = DATA_COLLECT
+            self.sync_count==0
+         else:
+            #should have been 2 SYNC_GAPS followed by SYNC interval
+            #  so <2 SYNC_GAPS followed by SYNC isn't our pattern: reset
+            self._reset()
+         self.need_pulse = True
+         return
+         
+      if ( self.state == DATA_COLLECT and token == GAP ):
+         print("DATA_COLLECT + GAP with bit_count = ", self.bit_count)
+         if self.bit_count==MSGLEN:
+            #This is a valid packet.  Send result back to caller and reset for next
+            self.cb(self.code, self.bit_count,
+                    int(self.pulseavg/self.pulsecnt),
+                    int(self.shortavg/self.shortcnt),
+                    int(self.longavg/self.longcnt))
+         else:
+            print("SYNC OK; data collected != 40 bits, so packet not valid; ignore packet")
+         #and reset machine in any case
+         self._reset()
+         return
+      
+      if ( self.state == DATA_COLLECT and token == SHORT) or ( self.state == DATA_COLLECT and token == LONG):
+         #This is a data bit, so record it
+         self.bit_count += 1
+         self.code <<= 1                   #make room for next bit
+         self.code += 1 if token == LONG else 0
+         self.need_pulse = True
+         return
+         
+      #by default, all other cases reset recognition machine
+      self._reset()
+      return
+   
+   def _get_state(self):
+      return self.state, self.sync_count, self.bit_count, self.code
+
+
+#   rx: A class to read wireless codes transmitted by 433 MHz transmitter
 class rx():
-   def __init__(self, pi, gpio, callback=None,
-                      min_bits=8, max_bits=40, glitch=150):
+   def __init__(self, pi, gpio, valid_pkt_callback=None, glitch=150):
       """
       Instantiate with the Pi and the GPIO connected to the wireless
-      receiver.
+      receiver on the pin specified by "gpio"
 
       If specified the callback will be called whenever a new code
       is received.  The callback will be passed the code, the number
       of bits, the length (in us) of the gap, short pulse, and long
       pulse.
 
-      Codes with bit lengths outside the range min_bits to max_bits
-      will be ignored.
-
       A glitch filter will be used to remove edges shorter than
-      glitch us long from the wireless stream.  This is intended
+      "glitch" us long from the wireless stream.  This is intended
       to remove the bulk of radio noise.
       """
+      #instantiate the recognition machine and record the valid-packet callback
+      self.m = mach(callback=valid_pkt_callback)
       self.pi = pi
       self.gpio = gpio
-      self.cb = callback
-      self.min_bits = min_bits
-      self.max_bits = max_bits
       self.glitch = glitch
 
-      self._in_code = False
-      self._edge = 0
-      self._code = 0
-      self._gap = 0
-
-      self._ready = False
-
+      for e in Timing_Table:
+         e[3] = int(e[2]*(1.0-TOLERANCE/100.))          #set low-bound for this interval type
+         e[4] = int(e[2]*(1.0+TOLERANCE/100.))          #set high-bound for this interval type
+#         print(e)
+         
       pi.set_mode(gpio, pigpio.INPUT)
       pi.set_glitch_filter(gpio, glitch)
       pi.set_pull_up_down(gpio, pigpio.PUD_DOWN)
       
-      self._last_edge_tick = pi.get_current_tick()
+      self._tick_count = 0
+      self._last_edge_tick = -1
       self._cb = pi.callback(gpio, pigpio.EITHER_EDGE, self._cbf)
+      
+   def _class_edge(self,e):
+      for t in Timing_Table:
+         if t[3] <= e <= t[4]:
+            return t[0]
+      return None
 
-   def _timings(self, e0, e1):
+   def _cbf(self, gpio, level, tick):
       """
-      Accumulates the short and long pulse length so that an
-      average short/long pulse length can be calculated. The
-      figures may be used to tune the transimission settings.
+      Recognizer for PPM codes received from 433MHz receivers.
+
+      Accumulates the data packet bit code from pulse-interval pairs, with constant pulse
+      durations and with the interval length determining the value of the bit represented.  
+
+      Edges are defined by rising and trailing edges of the electrical pulses.
+      An "edge" that is the plateau is the pulse; the valley "edge" that follows is
+      its interval.
+
+      The edge length, "edge_len", can be time, in microseconds, of the
+      duration of either the pulse or the inter-pulse interval. 
+      We only know that it's an edge.  We assume that the first edge seen is a
+      pulse (plateau defined by a rising electrical edge followed by its 
+      trailing falling electrical edge. The interval that follows the pulse is 
+      defined by that falling edge and the rising edge of the next electrical pulse. 
+
+      The first two sync bits have lengths (PULSE,INTERVAL)) followed by the third
+      sync bit (PULSE,SYNC).  After that, we should see (PULSE,SHORT) to
+      represent a "0" data bit or (PULSE,LONG) to represent a "1" data bit.  
+      The 40th bit is terminated by (PULSE,GAP) to signal the end of that packet.
+
+      The start of packet is recognized by a sync preamble of 3 pulses.
+      The end of packet is recognized when the 40th bit has been received and the gap
+      that follows is at least GAP usec long.
+
+      In short, the packet format is:
+         -  constant pulse width throughout the packet
+         -  3 sync pulses with intervals of nearly-equal duration after first two pulses
+         -  sync interval ("sync") following the third sync pulse and before the first data bit
+         -  data bit pulse followed by an interval: "short" gap = 0, "long" gap = 1
+         -  trailing pulse terminates the last data-bit interval
+         -  inter-packet interval (duration of at least GAP usec) before next packet
+         -  transmitter sends the data packet "repeat" times before concluding transmission
       """
-      if e0 < e1:
-         shorter = e0
-         longer = e1
-      else:
-         shorter = e1
-         longer = e0
 
-      if self._bits:
-         self._t0 += shorter
-         self._t1 += longer
-      else:
-         self._t0 = shorter
-         self._t1 = longer
-
-      self._bits += 1
-
-   def _calibrate(self, e0, e1):
-      """
-      The first pair of pulses is used as the template for
-      subsequent pulses.  They should be one short, one long, not
-      necessarily in that order.  The ratio between long and short
-      should really be 2 or more.  If less than 1.5 the pulses are
-      assumed to be noise.
-      """
-      self._bits = 0
-      self._timings(e0, e1)
-      self._bits = 0
-
-      ratio = float(self._t1)/float(self._t0)
-
-      if ratio < 1.5:
-         self._in_code = False
-
-      slack0 = int(0.3 * self._t0)
-      slack1 = int(0.2 * self._t1)
-
-      self._min_0 = self._t0 - slack0
-      self._max_0 = self._t0 + slack0
-      self._min_1 = self._t1 - slack1
-      self._max_1 = self._t1 + slack1
-
-   def _test_bit(self, e0, e1):
-      """
-      Returns the bit value represented by the sequence of pulses.
-
-      0: short long
-      1: long short
-      2: illegal sequence
-      """
-      self._timings(e0, e1)
-
-      if   ( (self._min_0 < e0 < self._max_0) and
-             (self._min_1 < e1 < self._max_1) ):
-         return 0
-      elif ( (self._min_0 < e1 < self._max_0) and
-             (self._min_1 < e0 < self._max_1) ):
-         return 1
-      else:
-         return 2
-
-   def _cbf(self, g, l, t):
-      """
-      Accumulates the code from pairs of short/long pulses.
-      The code end is assumed when an edge greater than 5 ms
-      is detected.
-      """
-      edge_len = pigpio.tickDiff(self._last_edge_tick, t)
-      self._last_edge_tick = t
-
-      if edge_len > 2750: # 5000 us, 5 ms.
-
-         if self._in_code:
-            if self.min_bits <= self._bits <= self.max_bits:
-               self._lbits = self._bits
-               self._lcode = self._code
-               self._lgap = self._gap
-               self._lt0 = int(self._t0/self._bits)
-               self._lt1 = int(self._t1/self._bits)
-               self._ready = True
-               if self.cb is not None:
-                  self.cb(self._lcode, self._lbits,
-                          self._lgap, self._lt0, self._lt1)
-
-         self._in_code = True
-         self._gap = edge_len
-         self._edge = 0
-         self._bits = 0
-         self._code = 0
-
-      elif self._in_code:
-
-         if self._edge == 0:
-            self._e0 = edge_len
-         elif self._edge == 1:
-            self._calibrate(self._e0, edge_len)
-
-         if self._edge % 2: # Odd edge.
-
-            bit = self._test_bit(self._even_edge_len, edge_len)
-            self._code = self._code << 1
-            if bit == 1:
-               self._code += 1
-            elif bit != 0:
-               self._in_code = False
-
-         else: # Even edge.
-
-            self._even_edge_len = edge_len
-
-         self._edge += 1
-
-   def ready(self):
-      """
-      Returns True if a new code is ready.
-      """
-      return self._ready
-
-   def code(self):
-      """
-      Returns the last received code.
-      """
-      self._ready = False
-      return self._lcode
-
-   def details(self):
-      """
-      Returns details of the last received code.  The details
-      consist of the code, the number of bits, the length (in us)
-      of the gap, short pulse, and long pulse.
-      """
-      self._ready = False
-      return self._lcode, self._lbits, self._lgap, self._lt0, self._lt1
-
-   def cancel(self):
-      """
-      Cancels the wireless code receiver.
-      """
-      if self._cb is not None:
-         self.pi.set_glitch_filter(self.gpio, 0) # Remove glitch filter.
-         self._cb.cancel()
-         self._cb = None
+      # every other rising/falling edge triggers an analysis of the interval length
+#      if gpio != self.gpio:
+#         return
+      edge_len = pigpio.tickDiff(self._last_edge_tick, tick)
+      self._last_edge_tick = tick
+      if self._last_edge_tick < 0:
+         self._last_edge_tick = tick
+         return
+      if level == 2 or edge_len > 11000:          # watchdog timer
+         edge_type = GAP
+         self.pi.set_watchdog(self.gpio,0)
+      elif level == TRAILING:                     # falling edge --> just saw pulse
+         edge_type = PULSE
+      else: 
+         edge_type = self._class_edge(edge_len)
+      if self.m.bit_count == MSGLEN:
+         self.pi.set_watchdog(self.gpio,11)
+#      print(States[self.m.state], edge_len, "NONE" if edge_type==None else Intervals[edge_type], "--> ", end="")
+      self.m._next(edge_type,edge_len)
+#      print(States[self.m.state])
 
 """
    tx: A class to transmit the wireless codes sent by 433 MHz wireless fobs.
    [HDT] modified for PPM: constant pulse width, variable inter-pulse gaps (marks)
 """
 class tx():
-   def __init__(self, pi, gpio, pulse=PULSE_WIDTH,repeats=REPEATS, bits=MSGLEN, gap=GAP,
-                t0=SHORT, t1=LONG, sync=SYNC):
+   def __init__(self, pi, gpio, pulse=Timing_Table[PULSE][2],
+                repeats=REPEATS, bits=MSGLEN, gap=Timing_Table[GAP][2],
+                t0=Timing_Table[SHORT][2], t1=Timing_Table[LONG][2],
+                sync=Timing_Table[SYNC][2]):
       """
       Instantiate with the Pi and the GPIO connected to the wireless
       transmitter on pin "gpio".
